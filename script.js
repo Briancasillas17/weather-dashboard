dbcde8b9c7051feceb3071c3836b2d9679a1a28c
Your Name <briancasillas67@yahoo.com>
2022-04-20 17:12:11 -0700

    added 5 days and dates

---
+++ script.js
@@ -0,0 +1,7 @@
+//Dates
+var startDate = moment().format('M/DD/YYYY');  // Current Date
+var day1 = moment().add(1, 'days').format('M/DD/YYYY');
+var day2 = moment().add(2, 'days').format('M/DD/YYYY');
+var day3 = moment().add(3, 'days').format('M/DD/YYYY');
+var day4 = moment().add(4, 'days').format('M/DD/YYYY');
+var day5 = moment().add(5, 'days').format('M/DD/YYYY');
